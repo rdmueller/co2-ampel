57b0c83d275ba64607151e40301efabea2f59cc4
Ralf D. Müller <ralf.d.mueller@gmail.com>
2020-11-11 23:02:13 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+function warte_bis_bereit () {
+    istBereit = 0
+    while (istBereit == 0) {
+        // checken, ob Werte anliegen
+        pins.i2cWriteNumber(
+        SCD30ADR,
+        GetDataReadyStatusCMD,
+        NumberFormat.UInt16BE,
+        false
+        )
+        // 4ms warten
+        control.waitMicros(3000)
+        istBereit = pins.i2cReadNumber(SCD30ADR, NumberFormat.UInt16BE, false)
+        control.waitMicros(3000)
+    }
+}
 function startMeasurement () {
     // checken, ob Werte anliegen
     pins.i2cWriteNumber(
@@ -15,25 +31,10 @@ function leseWert () {
     NumberFormat.UInt16BE,
     false
     )
-    // 4ms warten
+    // 3ms warten
     control.waitMicros(3000)
-    co2wert = pins.i2cReadNumber(SCD30ADR, NumberFormat.Float32BE, false)
-}
-function warte_bis_bereit () {
-    istBereit = 0
-    while (istBereit == 0) {
-        // checken, ob Werte anliegen
-        pins.i2cWriteNumber(
-        SCD30ADR,
-        GetDataReadyStatusCMD,
-        NumberFormat.UInt16BE,
-        false
-        )
-        // 4ms warten
-        control.waitMicros(3000)
-        istBereit = pins.i2cReadNumber(SCD30ADR, NumberFormat.UInt16BE, false)
-        control.waitMicros(3000)
-    }
+    co2wert = pins.i2cReadNumber(SCD30ADR, NumberFormat.Float32BE, true)
+    temperatur = input.temperature()
 }
 function getVersion () {
     // checken, ob Werte anliegen
@@ -45,8 +46,9 @@ function getVersion () {
     )
     basic.showNumber(pins.i2cReadNumber(SCD30ADR, NumberFormat.UInt16BE, false))
 }
-let istBereit = 0
+let temperatur = 0
 let co2wert = 0
+let istBereit = 0
 let StartPeriodicMeasurementCMD = 0
 let GetVersionCMD = 0
 let ReadMeasurementCMD = 0
@@ -59,13 +61,25 @@ GetVersionCMD = 53504
 StartPeriodicMeasurementCMD = 16
 warte_bis_bereit()
 basic.showString("CO2-AMPEL")
-// Protokollbeschreibung
-// 
+// Protokollbeschreibung des Sensors
 // https://www.sensirion.com/fileadmin/user_upload/customers/sensirion/Dokumente/9.5_CO2/Sensirion_CO2_Sensors_SCD30_Interface_Description.pdf
+// CO2 Bewertung
+// https://www.umweltbundesamt.de/sites/default/files/medien/pdfs/kohlendioxid_2008.pdf
 basic.forever(function () {
     warte_bis_bereit()
     startMeasurement()
     leseWert()
     basic.showString("CO2: ")
+    if (co2wert <= 800) {
+        basic.setLedColor(0x008000)
+    } else if (co2wert <= 1000) {
+        basic.setLedColor(0x408000)
+    } else if (co2wert <= 1400) {
+        basic.setLedColor(0x804000)
+    } else {
+        basic.setLedColor(0xff0000)
+    }
     basic.showNumber(co2wert)
+    basic.showString("TMP: ")
+    basic.showNumber(temperatur)
 })
