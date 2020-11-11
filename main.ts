f719605d395f7d971ce174b632a9c2596aadc4e7
Ralf D. Müller <ralf.d.mueller@gmail.com>
2020-11-11 21:48:14 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,48 +1,59 @@
 function leseWert () {
     // checken, ob Werte anliegen
     pins.i2cWriteNumber(
-    SC30ADR,
+    SCD30ADR,
     ReadMeasurementCMD,
-    NumberFormat.UInt16LE,
-    true
+    NumberFormat.UInt16BE,
+    false
     )
     // 4ms warten
-    control.waitMicros(4000)
-    co2wert = pins.i2cReadNumber(SC30ADR, NumberFormat.UInt32BE, true)
-    // 4ms warten
-    control.waitMicros(4000)
-    temperaturwert = pins.i2cReadNumber(SC30ADR, NumberFormat.UInt32BE, true)
-    // 4ms warten
-    control.waitMicros(4000)
-    luftfeuchtigkeitwert = pins.i2cReadNumber(SC30ADR, NumberFormat.UInt32BE, false)
+    control.waitMicros(3000)
+    co2wert = pins.i2cReadNumber(SCD30ADR, NumberFormat.UInt32BE, true)
+    temperaturwert = pins.i2cReadNumber(SCD30ADR, NumberFormat.UInt32BE, true)
+    luftfeuchtigkeitwert = pins.i2cReadNumber(SCD30ADR, NumberFormat.UInt32BE, false)
 }
 function warte_bis_bereit () {
     istBereit = 0
     while (istBereit == 0) {
         // checken, ob Werte anliegen
         pins.i2cWriteNumber(
-        SC30ADR,
+        SCD30ADR,
         GetDataReadyStatusCMD,
-        NumberFormat.UInt16LE,
-        true
+        NumberFormat.UInt16BE,
+        false
         )
         // 4ms warten
-        control.waitMicros(4000)
-        istBereit = pins.i2cReadNumber(SC30ADR, NumberFormat.UInt16BE, false)
+        control.waitMicros(3000)
+        istBereit = pins.i2cReadNumber(SCD30ADR, NumberFormat.UInt16BE, false)
         basic.showNumber(istBereit)
-        control.waitMicros(4000)
+        control.waitMicros(3000)
     }
+}
+function getVersion () {
+    // checken, ob Werte anliegen
+    pins.i2cWriteNumber(
+    SCD30ADR,
+    GetVersionCMD,
+    NumberFormat.UInt16BE,
+    false
+    )
+    basic.showNumber(pins.i2cReadNumber(SCD30ADR, NumberFormat.UInt16BE, false))
 }
 let istBereit = 0
 let luftfeuchtigkeitwert = 0
 let temperaturwert = 0
 let co2wert = 0
+let GetVersionCMD = 0
 let ReadMeasurementCMD = 0
 let GetDataReadyStatusCMD = 0
-let SC30ADR = 0
-SC30ADR = 97
+let SCD30ADR = 0
+let SCD30WRITE = 0
+let SCD30READ = 195
+SCD30WRITE += 194
+SCD30ADR = 97
 GetDataReadyStatusCMD = 514
 ReadMeasurementCMD = 768
+GetVersionCMD = 53504
 // Protokollbeschreibung
 // 
 // https://www.sensirion.com/fileadmin/user_upload/customers/sensirion/Dokumente/9.5_CO2/Sensirion_CO2_Sensors_SCD30_Interface_Description.pdf
