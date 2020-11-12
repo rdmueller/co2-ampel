6bb70db5fd148ecc77d7bc244487643757281997
Ralf D. Müller <ralf.d.mueller@gmail.com>
2020-11-12 15:01:37 +0100

    Werte werden jetzt auch über USB ausgegeben

--- main.ts
+++ main.ts
@@ -46,6 +46,19 @@ function getVersion () {
     )
     basic.showNumber(pins.i2cReadNumber(SCD30ADR, NumberFormat.UInt16BE, false))
 }
+function displayCO2 () {
+    wert = 50
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 4; y++) {
+            if (wert < co2wert) {
+                led.plot(x, y)
+            } else {
+                led.unplot(x, y)
+            }
+            wert += 100
+        }
+    }
+}
 let wert = 0
 let temperatur = 0
 let co2wert = 0
@@ -62,32 +75,27 @@ GetVersionCMD = 53504
 StartPeriodicMeasurementCMD = 16
 warte_bis_bereit()
 basic.showString("CO2-AMPEL")
+startMeasurement()
+serial.redirectToUSB()
+serial.setBaudRate(BaudRate.BaudRate9600)
 // Protokollbeschreibung des Sensors
 // https://www.sensirion.com/fileadmin/user_upload/customers/sensirion/Dokumente/9.5_CO2/Sensirion_CO2_Sensors_SCD30_Interface_Description.pdf
 // CO2 Bewertung
 // https://www.umweltbundesamt.de/sites/default/files/medien/pdfs/kohlendioxid_2008.pdf
 basic.forever(function () {
     warte_bis_bereit()
-    startMeasurement()
     leseWert()
+    serial.writeLine("")
+    serial.writeString("CO2: ")
+    serial.writeNumber(co2wert)
     if (co2wert <= 800) {
         basic.setLedColor(0x008000)
     } else if (co2wert <= 1000) {
-        basic.setLedColor(0x408000)
+        basic.setLedColor(0xffff00)
     } else if (co2wert <= 1400) {
-        basic.setLedColor(0x804000)
+        basic.setLedColor(0xff8000)
     } else {
         basic.setLedColor(0xff0000)
     }
-    wert = 0
-    for (let x = 0; x <= 4; x++) {
-        for (let y = 0; y <= 4; y++) {
-            if (wert < co2wert) {
-                led.plot(x, y)
-            } else {
-                led.unplot(x, y)
-            }
-            wert += 100
-        }
-    }
+    displayCO2()
 })
