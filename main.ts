0e5c9b95aff98cc6d3c168e69322521811f30ad0
Ralf D. Müller <ralf.d.mueller@gmail.com>
2020-11-25 23:07:39 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,40 +1,3 @@
-function readCalibration () {
-    pins.i2cWriteNumber(
-    SCD30ADR,
-    RecalibrationValueCMD,
-    NumberFormat.UInt16BE,
-    false
-    )
-    let buffer = pins.i2cReadBuffer(SCD30ADR, 2)
-serial.writeLine("buffer: " + ("" + buffer[0]) + "/" + ("" + buffer[1]))
-}
-function getVersion () {
-    pins.i2cWriteNumber(
-    SCD30ADR,
-    GetVersionCMD,
-    NumberFormat.UInt16BE,
-    false
-    )
-    version = pins.i2cReadNumber(SCD30ADR, NumberFormat.UInt16BE, false)
-    serial.writeLine("version: " + ("" + version))
-    pins.i2cWriteNumber(
-    SCD30ADR,
-    GetVersionCMD,
-    NumberFormat.UInt16BE,
-    false
-    )
-    let buffer2 = pins.i2cReadBuffer(SCD30ADR, 2)
-serial.writeLine("version2: " + ("" + buffer2[0]) + "/" + ("" + buffer2[1]))
-    serial.writeLine("version3: " + ("" + (buffer2[0] * 256 + buffer2[1])))
-    pins.i2cWriteNumber(
-    SCD30ADR,
-    GetVersionCMD,
-    NumberFormat.UInt16BE,
-    false
-    )
-    buffer2 = pins.i2cReadBuffer(SCD30ADR, 3)
-serial.writeLine("version2: " + ("" + buffer2[0]) + "/" + ("" + buffer2[1]) + "/" + ("" + buffer2[2]))
-}
 function displayCO2 () {
     wert = 50
     for (let x = 0; x <= 4; x++) {
@@ -53,9 +16,14 @@ let wert = 0
 let version = 0
 let RecalibrationValueCMD = 0
 let GetVersionCMD = 0
+bluetooth.startUartService()
+bluetooth.uartWriteLine("booting...")
 let temperatur = 0
 let istBereit = 0
 let SCD30ADR = 0
+serial.redirectToUSB()
+serial.setBaudRate(BaudRate.BaudRate9600)
+serial.writeLine("Starting CO2-Ampel")
 SCD30ADR = 97
 let GetDataReadyStatusCMD = 514
 let ReadMeasurementCMD = 768
@@ -63,10 +31,9 @@ GetVersionCMD = 53504
 let StartPeriodicMeasurementCMD = 16
 RecalibrationValueCMD = 20996
 basic.showString("CO2-AMPEL")
+bluetooth.uartWriteLine("CO2-Ampel aktiv")
 serial.redirectToUSB()
 serial.setBaudRate(BaudRate.BaudRate9600)
-readCalibration()
-getVersion()
 // Protokollbeschreibung des Sensors
 // https://www.sensirion.com/fileadmin/user_upload/customers/sensirion/Dokumente/9.5_CO2/Sensirion_CO2_Sensors_SCD30_Interface_Description.pdf
 // CO2 Bewertung
@@ -82,6 +49,7 @@ basic.forever(function () {
     serial.writeNumber(SCD30.readTemperature())
     serial.writeString(";")
     serial.writeNumber(SCD30.readHumidity())
+    bluetooth.uartWriteLine("CO2: " + co2wert + ", Temp: " + SCD30.readTemperature() + "Hum: " + SCD30.readHumidity())
     if (co2wert <= 800) {
         basic.setLedColor(0x008000)
     } else if (co2wert <= 1000) {
